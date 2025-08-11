30288c6aadf12c58a3780c24a6d577ca8e8c7da5
Lucifer-0612 <Ashuofficial1206@gmail.com>
2025-08-11 23:49:56 +0530

    added models files

---
+++ src/models/user.models.js
@@ -0,0 +1,91 @@
+
+import mongoose,{Schema} from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username:
+        {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true,
+            lowercase: true
+
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            //match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+            
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index:true
+        },
+        avatar:
+        {
+            type: String,
+            required:true
+        },
+        coverImage:
+        {
+            type:String
+
+        },
+        watchHistory:
+        {
+            type: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Video"
+                }
+            ],
+            default: []
+        },
+        password:
+        {
+            type: String,
+            required: [true,'password is required'],
+            select: false,
+            minlength: 8
+        },
+        refreshtoken:
+        {
+            type:String
+        }
+
+
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+userSchema.methods.isPasswordCorrect= async function (password)
+{
+    return await bcrypt.compare(password,this.password)
+}
+
+
+export const user = mongoose.model("User", userSchema)
---
+++ src/models/video.models.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new mongoose.Schema(
+    {
+        videofile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+           
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+module.exports = mongoose.model('Video', videoSchema);
