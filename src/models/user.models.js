b5dd3a37292e189fadfd77f058b55755c2ba8be4
Lucifer-0612 <Ashuofficial1206@gmail.com>
2025-08-12 23:49:18 +0530

    added methods and jwt files

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,6 @@
 
 import mongoose,{Schema} from "mongoose"
-import jwt from "jsonwebtoken"
+import jwt, { sign } from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
@@ -85,6 +85,36 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect= async function (password)
 {
     return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken= function()
+{
+   return jwt.sign(
+        {
+            _id : this._id,
+            username: this.username,
+            fullname: this.fullname,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken=function()
+{
+    return jwt.sign(
+        {
+            _id : this._id
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 
