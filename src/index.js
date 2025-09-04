4804e09a304649001088492a00cd8a6b3f04e70d
Lucifer-0612 <Ashuofficial1206@gmail.com>
2025-09-04 10:20:49 +0530

    added routers file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from './app.js';
 
 dotenv.config(
     {
-        path: './env'
+        path: './.env' 
     }
 )
 
 connectDB()
 .then(()=>
 {
-    app.on("error",(error)=>
+     app.on("error",(error)=>
     {
         console.log("error",error)
         throw error
-    })
+    }) 
     app.listen(process.env.PORT|| 8000,()=>
     {
         console.log("SERVER IS CONNECTED AND RUNNING")
@@ -22,7 +23,7 @@ connectDB()
 })
 .catch((err)=>
 {
-    console.error("MONGO DB CONNECTION FAILED", error)
+    console.error("MONGO DB CONNECTION FAILED", err)
 
 })
 
--- src/utils/asyncHandlers.js
+++ src/utils/asyncHandlers.js
@@ -1,6 +1,6 @@
 //this file is used to standardiz the format of response of request and error. we need to send everytime
 const asyncHandler = (requestHandler)=> {
-(req,res,next)=>{
+return (req,res,next)=>{
     Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
     {
 
